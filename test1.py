317b09e4d747aeaa764b3abd8c71671d37f98629
hui <hui.cheng@uis.no>
2022-06-24 16:21:10 +0200

    test the pytest method

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def add_two(x,y):
     return x+y
@@ -8,3 +8,13 @@ def accum_sum(x):
     for i in range(x):
         s+=i
     return s
+
+def test_accum():
+    assert accum_sum(1000)==19
+
+if __name__ == "__main__":
+    a1=int(input('Please input the first number\n'))
+    a2=int(input('Please input the second number\n'))
+    print(add_two(a1,a2))
+    print()
+    print(accum_sum(a2))
